db9952a5d34613723ace023bb16f2f2676c22c5e
marck antonio <marckfloresantonio@gmail.com>
2024-10-26 04:56:28 +0800

    update

---
+++ SamplePizza.Data/Data/ContosoPizzaContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using SamplePizza.Models;
+
+namespace SamplePizza.Data
+{
+    public partial class SamplePizzaContext : DbContext
+    {
+        public SamplePizzaContext()
+        {
+ 
+        }
+
+        public SamplePizzaContext(DbContextOptions<SamplePizzaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; } = null!;
+        public virtual DbSet<Order> Orders { get; set; } = null!;
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Pizza_db;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ SamplePizza.Data/Program.cs
@@ -0,0 +1,21 @@
+﻿using SamplePizza.Data;
+using SamplePizza.Models;
+
+using SamplePizzaContext context = new SamplePizzaContext();
+
+Product veggieSpecial = new Product()
+{
+    Name = "Veggie Special Pizza",
+    Price = 9.99M
+};
+context.Products.Add(veggieSpecial);
+
+Product deluxeMeat = new Product()
+{
+    Name = "Deluxe Meat Pizza",
+    Price = 12.99M
+};
+context.Add(deluxeMeat);
+
+context.SaveChanges();
+
